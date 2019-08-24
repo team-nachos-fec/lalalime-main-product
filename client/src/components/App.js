6ab84e7331493575b8ff082ca703a5ba797ad9d4
Dennis <dcmclc@gmail.com>
2019-08-24 16:44:53 -0700

    Implemented GET request on component mount

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
     constructor(props) {
@@ -11,12 +12,33 @@ class App extends React.Component {
             sizes: [],
             breadcrumbs: []
         }
+        this.componentDidMount = this.componentDidMount.bind(this);
     }
 
+    // Get a specific product from database
+    componentDidMount() {
+        axios.get('/api/products')
+        .then((response) => {
+            console.log('Yay got data', response.data[0])
+            this.setState({
+                images: response.data[0].images,
+                name: response.data[0].name,
+                price: response.data[0].price,
+                description: response.data[0].description,
+                sizes: response.data[0].sizes,
+                breadcrumbs: response.data[0].breadcrumbs
+            })
+            console.log('State has been set', this.state);
+        })
+        .catch((err) => {
+            console.error('Uh oh did not work', err)
+        })
+    }
 
+    
     render() {
         return (
-            <div>
+            <div onClick={this.getProducts}>
                 Rendering the App! Woo
             </div>
         )
