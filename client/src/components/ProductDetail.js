253218e528caad663c59331d961626081c0b2977
Dennis <dcmclc@gmail.com>
2019-08-26 18:03:11 -0700

    Positioning product detail component

--- client/src/components/ProductDetail.js
+++ client/src/components/ProductDetail.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const ProductDetail = (props) => {
     return (
-        <div>
-            <div>{props.name}</div>
-            <span>{props.price}</span>
+        <div className="product-detail-box">
+            <div className="product-name">{props.name}</div>
+            <span className="product-price">{props.price}</span>
             <h2>Why we made this</h2>
-            <p>{props.description}</p>
+            <p className="product-description">{props.description}</p>
         </div>
     )
 }
