9fcb6b469fe5e188821291a58ec3bd75efdff1f0
Dennis <dcmclc@gmail.com>
2019-08-27 18:01:51 -0700

    Added google fonts to product description

--- client/src/components/ProductDetail.js
+++ client/src/components/ProductDetail.js
@@ -5,7 +5,7 @@ const ProductDetail = (props) => {
         <div className="product-detail-box">
             <div className="product-name">{props.name}</div>
             <span className="product-price">{props.price}</span>
-            <h2>Why we made this</h2>
+            <h2 className="wwmt">Why we made this</h2>
             <p className="product-description">{props.description}</p>
         </div>
     )
