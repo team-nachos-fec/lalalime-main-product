1c7afa3876554c9705e14eb0783f01124df64f69
Dennis <dcmclc@gmail.com>
2019-08-30 16:16:45 -0700

    Developing dropdown content for fabric box

--- client/src/components/FabricInfo.js
+++ client/src/components/FabricInfo.js
@@ -6,6 +6,17 @@ const FabricInfo = (props) => {
             <div className="fabric-box">
                 <h3 className="fabric-text">Fabric</h3>
                 <h3 className="fabric-plus">+</h3>
+                <div id="fabric-dropdown" className="fabric-content">
+                    <p className="trademark">Nulu™</p>
+                    <p className="fabric-para">Lightweight, buttery-soft Nulu™ fabric wicks sweat and is four-way stretch</p>
+                    <hr className="fabric-line"/>
+                    <div className="fabric-list">
+                        <div>Buttery-Soft Handfeel</div>
+                        <div>Lightweight</div>
+                        <div>Sweat-Wicking</div>
+                        <div>Four-Way Stretch</div>
+                    </div>
+                </div>
             </div>
             <div className="care-box">
                 <h3 className="fabric-text">Care</h3>
